9a3e19087eff16e8adccad3f1f4c913c1b353b9f
masaya.suzuki <suzukimasaya428@gmail.com>
2025-07-27 13:43:53 +0900

    fix

--- scripts/deploy_hato_bot/update_version_python_version/get_python_version.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-
-cp .env.example .env
-export TAG_NAME="${HEAD_REF//\//-}"
-docker compose pull
-DOCKER_CMD="python --version 2>&1 | sed -e 's/^Python //g'"
-python_version=$(docker compose run hato-bot-go sh -c "${DOCKER_CMD}")
-echo "Python version:" "${python_version}"
-sed -i -e "s/requires-python = \"==.*\"/requires-python = \"==${python_version}\"/g" pyproject.toml
