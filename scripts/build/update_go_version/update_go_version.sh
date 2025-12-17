f4ae00a182c8776af1704fbe10f86d35b4ce19ea
Masaya Suzuki <15100604+massongit@users.noreply.github.com>
2025-12-18 07:33:47 +0900

    正規表現修正

--- scripts/build/update_go_version/update_go_version.sh
+++ scripts/build/update_go_version/update_go_version.sh
@@ -2,5 +2,5 @@
 set -e
 
 action="$(yq '.jobs.pr-super-lint.steps[-1].uses | line_comment' .github/workflows/pr-test.yml)"
-go_version=$(docker run --rm --entrypoint '' "ghcr.io/super-linter/super-linter:slim-${action}" /bin/sh -c 'go version' | sed -e 's/go version go\([0-9.]*\) .*$/\1/g')
+go_version=$(docker run --rm --entrypoint '' "ghcr.io/super-linter/super-linter:slim-${action}" /bin/sh -c 'go version' | sed -e 's/^go version go\([0-9.]*\) .*$/\1/g')
 sed -i -e "s/^go \([0-9.]*\)/go $go_version/g" go.mod
