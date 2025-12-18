913241ef69e48d77c2ab9f1a27477f9d11ef7ce8
Masaya Suzuki <15100604+massongit@users.noreply.github.com>
2025-12-18 09:24:24 +0900

    goのバージョンをsuper-linterのものにする (#213)
    * goのバージョンをsuper-linterのものにする
    
    * 正規表現修正
    
    * CI update-go-versionを流用する
    
    * renovate: golangをアップデート対象から外す
    
    * 正規表現修正

--- scripts/build/update_go_version/update_go_version.sh
+++ scripts/build/update_go_version/update_go_version.sh
@@ -1,5 +1,6 @@
 #!/usr/bin/env bash
 set -e
 
-go_version=$(grep golang: Dockerfile | sed -e 's!FROM golang:\([0-9.]*\)-.*!\1!g')
+action="$(yq '.jobs.pr-super-lint.steps[-1].uses | line_comment' .github/workflows/pr-test.yml)"
+go_version=$(docker run --rm --entrypoint '' "ghcr.io/super-linter/super-linter:slim-${action}" /bin/sh -c 'go version' | sed -e 's/^go version go\([0-9.]*\) .*$/\1/g')
 sed -i -e "s/^go \([0-9.]*\)/go $go_version/g" go.mod
