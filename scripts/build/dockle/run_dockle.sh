f43a8468b6c8bbdd2a196e413c585a710e4983d3
masaya.suzuki <suzukimasaya428@gmail.com>
2026-03-15 14:51:46 +0900

    run_dockle.shをmisskey・mixi2の各docker-compose.ymlに対応させる

--- scripts/build/dockle/run_dockle.sh
+++ scripts/build/dockle/run_dockle.sh
@@ -6,11 +6,13 @@ export TAG_NAME="${HEAD_REF//\//-}"
 dockle_version="$(cat .dockle-version)"
 curl -L -o dockle.deb "https://github.com/goodwithtech/dockle/releases/download/v${dockle_version}/dockle_${dockle_version}_Linux-64bit.deb"
 sudo dpkg -i dockle.deb
-docker compose pull
-docker compose up -d
 
-for image_name in $(docker compose images | awk 'OFS=":" {print $2,$3}' | tail -n +2); do
-	cmd="dockle --exit-code 1 ${image_name}"
-	echo "> ${cmd}"
-	eval "${cmd}"
+for filename in misskey.docker-compose.yml mixi2.docker-compose.yml; do
+  docker compose -f docker-compose.yml -f "$filename" pull
+  docker compose -f docker-compose.yml -f "$filename" up -d
+  for image_name in $(docker compose -f docker-compose.yml -f "$filename" images | awk 'OFS=":" {print $2,$3}' | tail -n +2); do
+    cmd="dockle --exit-code 1 ${image_name}"
+    echo "> ${cmd}"
+    eval "${cmd}"
+  done
 done
